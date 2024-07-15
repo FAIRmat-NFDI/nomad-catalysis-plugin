f07a6a3e530de827c7a1060269d4c06ee00b4af4
Julia Schumann <32182254+schumannj@users.noreply.github.com>
2024-07-15 15:42:30 +0000

    add app

--- src/nomad_catalysis/apps/__init__.py
+++ src/nomad_catalysis/apps/__init__.py
@@ -1,24 +1,11 @@
 from nomad.config.models.plugins import AppEntryPoint
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
 
+from nomad_catalysis.apps.heterogeneous_catalysis_app import heterogeneous_catalysis_app
 
 myapp = AppEntryPoint(
-    name='MyApp',
-    description='App defined using the new plugin mechanism.',
-    app=App(
-        label='MyApp',
-        path='myapp',
-        category='simulation',
-        columns=Columns(
-            selected=['entry_id'],
-            options={
-                'entry_id': Column(),
-            },
-        ),
-        filter_menus=FilterMenus(
-            options={
-                'material': FilterMenu(label='Material'),
-            }
-        ),
-    ),
+    name='Catalysis App',
+    description="""
+    This app allows you to search **heterogeneous catalysis data** within NOMAD.
+    """,
+    app=heterogeneous_catalysis_app
 )
