14e680c536dccd6c04854c5ba6822e7eead303c4
Julia Schumann <jul.schumann86@gmail.com>
2024-07-23 11:45:10 +0200

    remove example package and replace with catalysis

--- src/nomad_catalysis/schema_packages/__init__.py
+++ src/nomad_catalysis/schema_packages/__init__.py
@@ -2,32 +2,16 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
 
 
-class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
-
-    def load(self):
-        from nomad_catalysis.schema_packages.mypackage import m_package
-
-        return m_package
-
-
-mypackage = MySchemaPackageEntryPoint(
-    name='MyPackage',
-    description='Schema package defined using the new plugin mechanism.',
-)
-
-
 class CatalysisPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from nomad_catalysis_test.schema_packages.schema import m_package
+        from nomad_catalysis.schema_packages.schema import m_package
 
         return m_package
 
 
 catalysis = CatalysisPackageEntryPoint(
     name='Catalysis',
-    description='Catalysis Schema package '
-    'defined using the new plugin mechanism.',
+    description='Catalysis Schema package defined using the new plugin mechanism.',
 )
