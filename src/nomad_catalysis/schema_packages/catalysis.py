2441dfa407f6349a8821399cb8c475a70e9ed5f3
Julia Schumann <32182254+schumannj@users.noreply.github.com>
2024-11-12 15:52:35 +0100

    Small fixes (#28)
    * small fix data reduction and duplicate figures in reaction conditions
    
    * continue fix
    
    * fix ruff linting

--- src/nomad_catalysis/schema_packages/catalysis.py
+++ src/nomad_catalysis/schema_packages/catalysis.py
@@ -62,6 +62,7 @@ class CatalysisElnCategory(EntryDataCategory):
 
 
 threshold_datapoints = 300
+threshold2_datapoints = 3000
 
 
 def add_catalyst(archive: 'EntryArchive') -> None:
@@ -150,7 +151,10 @@ def map_and_assign_attributes(self, logger, mapping, target, obj=None) -> None:
                         f"""The quantity '{ref_attr}' is large and will be reduced for
                         the archive results."""
                     )
-                    value = value[50::100]
+                    if threshold_datapoints < len(value) < threshold2_datapoints:
+                        value = value[20::10]
+                    else:
+                        value = value[50::100]
             except TypeError:
                 pass
             try:
@@ -986,6 +990,7 @@ class ReactionConditionsData(PlotSection):
     reagents = SubSection(section_def=Reagent, repeats=True)
 
     def plot_figures(self):
+        self.figures = []
         if self.time_on_stream is not None:
             x = self.time_on_stream.to('hour')
             x_text = 'time (h)'
@@ -1567,7 +1572,8 @@ class CatalyticReaction(CatalyticReactionCore, PlotSection, Schema):
                     header['Particle size (Dp) [mm]'] * ureg.millimeter
                 )
 
-                self.experimenter = header['User'][0].decode()
+                if not self.experimenter:
+                    self.experimenter = header['User'][0].decode()
 
                 pre = data['Sorted Data'][methodname]['H2 Reduction']
                 pretreatment.set_temperature = (
@@ -2221,7 +2227,11 @@ class CatalyticReaction(CatalyticReactionCore, PlotSection, Schema):
                         if key == 'name':
                             continue
                         if getattr(react, key) is not None:
-                            setattr(react, key, getattr(react, key)[50::100])
+                            if len(getattr(react, key1)) > threshold2_datapoints:
+                                setattr(react, key, getattr(react, key)[50::100])
+                            else:
+                                setattr(react, key, getattr(react, key)[20::10])
+
                 break
         return react
 
@@ -2299,16 +2309,8 @@ class CatalyticReaction(CatalyticReactionCore, PlotSection, Schema):
             return
         product_results = []
         for i in self.results[0].products:
-            if (
-                i.pure_component is not None
-                and i.pure_component.iupac_name is not None
-                and i.pure_component.iupac_name != 'azane'
-            ):
+            if i.pure_component is not None and i.pure_component.iupac_name is not None:
                 i.name = i.pure_component.iupac_name
-            elif (
-                i.pure_component is not None and i.pure_component.iupac_name == 'azane'
-            ):
-                i.name = 'ammonia'
             prod = Product(
                 name=i.name,
                 selectivity=i.selectivity,
@@ -2316,7 +2318,10 @@ class CatalyticReaction(CatalyticReactionCore, PlotSection, Schema):
                 space_time_yield=i.space_time_yield,
             )
             if i.selectivity is not None and len(i.selectivity) > threshold_datapoints:
-                prod.selectivity = i.selectivity[50::100]
+                if threshold2_datapoints > len(i.selectivity):
+                    prod.selectivity = i.selectivity[20::10]
+                else:
+                    prod.selectivity = i.selectivity[50::100]
                 logger.info(
                     f"""Large arrays in product {i.name}, reducing to store
                     in the archive."""
@@ -2325,12 +2330,18 @@ class CatalyticReaction(CatalyticReactionCore, PlotSection, Schema):
                 i.gas_concentration_out is not None
                 and len(i.gas_concentration_out) > threshold_datapoints
             ):
-                prod.gas_concentration_out = i.gas_concentration_out[50::100]
+                if threshold2_datapoints > len(i.gas_concentration_out):
+                    prod.gas_concentration_out = i.gas_concentration_out[20::10]
+                else:
+                    prod.gas_concentration_out = i.gas_concentration_out[50::100]
             if (
                 i.space_time_yield is not None
                 and len(i.space_time_yield) > threshold_datapoints
             ):
-                prod.space_time_yield = i.space_time_yield[50::100]
+                if threshold2_datapoints > len(i.space_time_yield):
+                    prod.space_time_yield = i.space_time_yield[20::10]
+                else:
+                    prod.space_time_yield = i.space_time_yield[50::100]
 
             product_results.append(prod)
 
