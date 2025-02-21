fe464b4c4f56e212738ee8aaffcfedfd74c2ac5f
Julia Schumann <jul.schumann86@gmail.com>
2025-02-21 15:26:43 +0100

    improve readme

--- src/nomad_catalysis/example_uploads/__init__.py
+++ src/nomad_catalysis/example_uploads/__init__.py
@@ -3,6 +3,9 @@ from nomad.config.models.plugins import ExampleUploadEntryPoint
 catalysis = ExampleUploadEntryPoint(
     title='Heterogeneous Catalysis Example',
     category='FAIRmat examples',
-    description='Set examples files for catalyst sample and catalytic reaction',
+    description=(
+        'Examples files for catalyst sample and catalytic reaction to demonstrate '
+        'the new classes from the nomad-catalysis plugin.'
+    ),
     resources=['example_uploads/het_catalysis_example/*'],
 )
